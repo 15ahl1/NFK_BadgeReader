2751e8180f5b98c821ac092e68a613fbab69c807
root <alastair.lewis10@gmail.com>
2020-02-06 14:51:14 -0500

    Simplified Record Maker

--- rpi-scanner/RecordMaker.py
+++ rpi-scanner/RecordMaker.py
@@ -2,21 +2,20 @@ from datetime import datetime
 import os
 
 class RecordMaker:
-    #Creates link to the machine class and the kafka class
-    def __init__(self, machineInput, kafkaInput):
-        global machine = machineInput
-        global kakfa = kafkaInput
+    usage = ""
+    #recieves a reference to the inUse machine variable
+    def __init__(self, inUse):
+        usage = inUse
 
-    #Creates and sends a record from the badge data
+    #Creates a record from the badge data
     def createRecord(self, facilityCode, badgeNumb):
         mac = os.system("LANG=C ifconfig -a | grep -Po 'HWaddr \K.*$'")
         time = datetime.now()
         id = facilityCode + "-" + badgeNumb
-        inUse = self.machine.inUse
-        kafka.sendRecord(mac + "," + time + "," + id + "," + inUse)
+        return(mac + "," + time + "," + id + "," + usage)
 
-    #Creates and sends a error message on a bad read
+    #Creates a error message on a bad read
     def createError(self, message):
         mac = os.system("LANG=C ifconfig -a | grep -Po 'HWaddr \K.*$'")
         time = datetime.now()
-        kakfa.sendRecord("Error: " + mac + " at time: " + time)
+        return("Error on machine: " + mac + " at: " + time)
