5e365a42d577207737292b2a3bc3612423b3456a
15ahl1 <alastair.lewis10@gmail.com>
2020-03-04 15:23:17 -0500

    Added a driver script for Client

--- host/Consumer.py
+++ host/Consumer.py
@@ -2,16 +2,19 @@
 #Consumes messages on localhost and dumps into a file
 import os
 import socket
+from Database import Database
 
 class Consumer:
     path = ""
     port = -1
+    database = None
 
     #Instantiate with path to output file and port to listen on
     #Needs to be an Absolute Path to get proper saving functionality
-    def __init__(self, path, port):
+    def __init__(self, path, port, database):
         self.path = path
         self.port = port
+        self.database = database
 
     #Call this function once and it will continuously process messages
     def consume(self):
@@ -22,22 +25,26 @@ class Consumer:
 
         while True:
             #Waiting for connection
-            s.listen(1)
+            s.listen(10)
             print("Waiting for a connection...")
             connection, client_address = s.accept()
 
             #Accepting connection
             print('Connection Accepted from: ' + str(client_address[0]))
-            message = connection.recv(1024)
-            print("Recieved message: " + message.decode())
+            message = connection.recv(1024).decode()
+            print("Recieved message: " + message)
+
+            #Send to Database
+            self.database.writeRecord(message)
 
             #Write to file
             with open(self.path, 'a') as f:
-                f.write(message.decode() + "\n")
+                f.write(message + "\n")
                 
             print("Message saved")
             message = ""
 
 #Needs to be an Absolute Path to get proper saving functionality
-c = Consumer("C:/Users/15ahl1/Desktop/Capstone/test.txt", 6969)
+d = Database("C:/Users/Alastair/Desktop/CISC 498/Capstone/test.txt")
+c = Consumer("C:/Users/Alastair/Desktop/CISC 498/Capstone/test.txt", 6969, d)
 c.consume()
