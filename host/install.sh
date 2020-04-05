6306b957b663cad01e60dd0702656b80d873118a
15ahl1 <alastair.lewis10@gmail.com>
2020-04-05 15:26:32 -0400

    added more to install script

--- host/install.sh
+++ host/install.sh
@@ -5,4 +5,9 @@ sudo pip3 install flask
 sudo pip3 install flask_wtf
 sudo pip3 install flask_mysqldb
 sudo pip3 install pandas
+sudo pip3 install openpyxl
+service mysql stop
+service mysql start
+mysql -uroot
+
 echo "Installation Complete"
