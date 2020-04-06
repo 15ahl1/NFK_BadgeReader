78d3319730d14bfbc16ddc651576c8a4991a006c
Michael Reinart <michaelreinhart112@gmail.com>
2020-04-06 13:36:00 -0400

    Update flaskapp.sql

--- host/flaskapp.sql
+++ host/flaskapp.sql
@@ -116,8 +116,6 @@ CREATE TABLE sessions (
   userName varchar(50)
 );
 
-INSERT INTO sessions(machineID,machineName,sessionStart,sessionEnd,timeUsed,rateUsed,rateTypeUsed,billAmount,userID,userName)
-VALUES("98:01:a7:8f:00:99","Oxford Lasers Micromachining Laser","2020-03-29 00:09:40","2020-03-29 00:012:40",3.45,"22","Academic",23.33,"12345", "Michael Reinhart");
 
 INSERT INTO machines(machine, name, inUse, academicRate, institutionalRate) VALUES('98:01:a7:8f:00:99', 'Oxford Lasers Micromachining Laser', '0',66.99, 99.25);
 INSERT INTO machines(machine, name, inUse, academicRate, institutionalRate) VALUES('b8:27:eb:61:98:05', 'Raith Pioneer Electron-beam', '0', 6, 99);
@@ -127,33 +125,3 @@ INSERT INTO machines(machine, name, inUse, academicRate, institutionalRate) VALU
 INSERT INTO machines(machine, name, inUse, academicRate, institutionalRate) VALUES('00:00:00:00:00:06', 'PVD 75 Sputtering System','0',66, 99);
 INSERT INTO machines(machine, name, inUse, academicRate, institutionalRate) VALUES('00:00:00:00:00:07', 'Thermionics electron-beam Evaporator', '0', 66, 99);
 
-INSERT INTO `users` (`username`, `supervisor`, `department`, `faculty`, `institution`, `rateType`, `Permissions`) VALUES ('michael', 'bob', 'computer science', 'cs', 'queens', '33', '1010101');
-
-
-INSERT INTO `supervisors` (`superName`) VALUES ('Graham Gibson');
-INSERT INTO `departments` (`deptName`) VALUES ('Queen\'s University');
-INSERT INTO `departments` (`deptName`) VALUES ('st lawrence');
-INSERT INTO `faculty` (`facultyName`) VALUES ('CMC');
-INSERT INTO `institution` (`institutionName`) VALUES ('CMC');
-INSERT INTO `rateType` (`ratename`, `rateAmount`) VALUES ('Regular Usage', "22");
-INSERT INTO `rateType` (`ratename`, `rateAmount`) VALUES ('Service Project', "33");
-
-
-INSERT INTO openCardNumber(cardNumber, timeUsed) values ('12345676766','2020-03-29 00:09:40');
-INSERT INTO openCardNumber(cardNumber, timeUsed) values ('8585','2020-08-29 00:09:40');
-
-
-INSERT INTO alerted(machine, machinename, timeUsed, userID, userName) VALUES ('98:01:a7:8f:00:99',"Oxford Lasers Micromachining Laser", "2020-03-29 00:09:40", "8585", "Mac Furlong");
-
-
--- Three Sessions
-INSERT INTO entries(machine,timeUsed,userID,inUse,enteredSession) VALUES ('98:01:a7:8f:00:99',"2020-03-31 00:09:40","12345",1,1);
-INSERT INTO entries(machine,timeUsed,userID,inUse,enteredSession) VALUES ('98:01:a7:8f:00:99',"2020-03-31 03:12:40","12345",0,1);
-
-
-INSERT INTO entries(machine,timeUsed,userID,inUse,enteredSession) VALUES ('98:01:a7:8f:00:99',"2020-04-05 00:09:40","12345",1,0);
-INSERT INTO entries(machine,timeUsed,userID,inUse,enteredSession) VALUES ('98:01:a7:8f:00:99',"2020-04-05 05:14:40","12345",0,0);
-
-
-INSERT INTO entries(machine,timeUsed,userID,inUse,enteredSession) VALUES ('b8:27:eb:61:98:05',"2020-04-02 00:012:40","42132",1,0);
-INSERT INTO entries(machine,timeUsed,userID,inUse,enteredSession) VALUES ('b8:27:eb:61:98:05',"2020-04-02 02:17:40","42132",0,0);
